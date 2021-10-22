cc93c92dcbcf3a32b5045b6ac053eac4ae064a3a
bala74923 <bala74923@gmail.com>
2021-10-22 14:27:20 +0530

    Working

---
+++ RedHut_review1/src/test/java/com/example/demo/RedHutReview1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedHutReview1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
